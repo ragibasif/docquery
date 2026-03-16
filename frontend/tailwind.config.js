1f9850901544c6f3f57eb5d8419348203e067f0e
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2026-03-16 19:05:58 -0400

    style: add daisyui to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,23 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
+
+  plugins: [
+    require("daisyui"),
+  ],
+
+  daisyui: {
+    themes: ["luxury --default --prefersdark", "light"],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: false,
   },
-  plugins: [],
 };
